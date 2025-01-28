c36f9a39df42ad29ddb00f7ecb6020feb29e946c
qwertycxz <gmail2@qq.com>
2025-01-28 12:08:19 +0800

    chore: perttier plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,12 @@
 import js from "@eslint/js";
 import ts from "typescript-eslint";
+import prettier from "eslint-plugin-prettier/recommended";
 export default ts.config({
     extends: [
         js.configs.recommended,
         ...ts.configs.recommendedTypeChecked,
         ...ts.configs.stylisticTypeChecked,
+        prettier,
     ],
     ignores: ["*.d.ts", "*.js"],
     languageOptions: {
